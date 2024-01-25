82497b9cda2c831fc8e78b45da5af7b7ae8d0901
Fred França <74054753+fredrdh@users.noreply.github.com>
2024-01-25 15:00:32 +0000

    Card Extraction

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import './App.css'
+import Card from './components/Card/Card'
 
 function App() {
   return (
     <>
-      <div className='card'>
-        <h2>Fry</h2>
-        <img src="https://comicvine.gamespot.com/a/uploads/square_small/11/114183/6665973-%24%20%282%29.png" />
-      </div>
+      <Card />
+      <Card />
     </>
   )
 }
---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,12 @@
+function Card() {
+    return (
+        <>
+          <div className='card'>
+            <h2>Fry</h2>
+            <img src="https://comicvine.gamespot.com/a/uploads/square_small/11/114183/6665973-%24%20%282%29.png" />
+          </div>
+        </>
+    )
+}
+
+export default Card
